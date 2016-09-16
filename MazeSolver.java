d70a294693b7df89c79a5daed39ad2d622055f75
unternehmen <unternehmen@users.noreply.github.com>
2016-09-16 15:51:39 -0400

    Finished parts other than picking a path.
    Next, I might move position checking code into a method called getNextUnexploredPosition which will do the proper checks and return a position or null.  It will serve the purpose of goEast/goWest...

--- MazeSolver.java
+++ MazeSolver.java
@@ -5,15 +5,22 @@
  * maze.getWidth() -> int
  * maze.getHeight() -> int
  */
+import java.util.Stack;
 
 public class Solver
 {
     private Maze maze;
     private int pos[3];
+    private Stack<int[]> trail;
     boolean solved;
     
-    private static boolean goNorth() {
+    private boolean isVictoryTile(int x, int y, int z) {
+        return maze.getTileAt(x, y, z) == '*';
+    }
+    
+    private boolean goNorth() {
         if (pos[1] > 0 && maze.getTileAt(pos[0], pos[1] - 1, pos[2]) != '#') {
+            trail.push(pos);
             pos[1]--;
             return true;
         }
@@ -21,9 +28,10 @@ public class Solver
         return false;
     }
     
-    private static boolean goSouth() {
+    private boolean goSouth() {
         if (pos[0] < maze.getHeight() - 1
             && maze.getTileAt(pos[0], pos[1] + 1, pos[2]) != '#') {
+            trail.push(pos);
             pos[1]++;
             return true;
         }
@@ -31,23 +39,26 @@ public class Solver
         return false;
     }
     
-    private static boolean goEast() {
+    private boolean goEast() {
         if (pos[0] < maze.getWidth() - 1
             && maze.getTileAt(pos[0] + 1, pos[1], pos[2]) != '#') {
+            trail.push(pos);
             pos[0]++;
             return true;
         }
     }
     
-    private static boolean goWest() {
+    private boolean goWest() {
         if (pos[0] > 0 && maze.getTileAt(pos[0] - 1, pos[1], pos[2]) != '#') {
+            trail.push(pos);
             pos[0]--;
             return true;
         }
     }
     
-    private static boolean goThrough() {
+    private boolean goThrough() {
         if (pos[2] > 0 && maze.findDisplacement(pos[0], pos[1], pos[2]) != '#') {
+            trail.push(pos);
             pos[0]--;
             return true;
         }
@@ -66,16 +77,41 @@ public class Solver
     */
     
     public Solver(Maze maze) {
+        int d, h, w;
+        
         this.pos = maze.getEntrance();
         this.maze = maze;
         this.solved = false;
+        this.trail = new Stack<int[]>();
+        
+        d = maze.getDepth();
+        h = maze.getHeight();
+        w = maze.getWidth();
+        this.explored = new boolean[d][h][w];
+    }
+    
+    private boolean isPathAvailable() {
+        /* Return whether there is an unexplored, passable path from the current position. */
     }
     
     public int[] next() {
-        /* If we are standing on the victory tile, the maze is solved.  Return null and set solved to true. */
-        /* Else: If there are unexplored paths from the current position, follow one of them and return that position. */
-        /* Else: If there are no unexplored paths, go back to the previous position and return that. */
-        /* Else: If there is no previous position, the maze is impossible to solve.  Return null. */
+        /* Mark the current tile as explored. */
+        explored[pos[2]][pos[1]][pos[0]] = true;
+
+        if (isVictoryTile(pos[0], pos[1], pos[2])) {
+            /* The maze is solved. */
+            solved = true;
+            return null;
+        } else if (isPathAvailable()) {
+            /* Follow one of the available paths and return that position. */
+        } else if (!trail.empty()) {
+            /* Go back to the previous position and return that. */
+            pos = trail.pop();
+            return pos;
+        } else {
+            /* The maze has been proven impossible to solve. */
+            return null;
+        }
     }
     
     public boolean isSolved() {
